00f813787b63fde284499d70f1235cf772e8533f
Zachary Feuerstein <zackfeuerstein@gmail.com>
2014-05-15 23:44:44 -0400

    final code

--- RobotControl/TestCode/HBTest/HBTest.ino
+++ RobotControl/TestCode/HBTest/HBTest.ino
@@ -1,13 +1,32 @@
+#include <movement.h>
+
+
+//Testing for the motors
+
+//----------------------------------------------------------------------------
+// Movement Setup
+//
+#define RME 8 //Right Motor Enable
+#define RMD 23 //Right Motor Direction
+#define RMQA 25 //Right Motor input A from quadrature-encoder
+#define RMQB 27 //Right Motor input B from quadrature-encoder
+//
+#define LME 9 //Same stuff just with left motor
+#define LMD 22
+#define LMQA 24
+#define LMQB 26
+//
+#define Speed 180
+//
+movement Robot(RMD, RME, RMQA, RMQB, LMD, LME, LMQA, LMQB, Speed);
+//
+//----------------------------------------------------------------------------
 
 
 void setup(){
-
-  pinMode(11,OUTPUT);
-
 }
 void loop(){
 
-   analogWrite(11, 200);
-
+  Robot.goXinches(1);
 
 }
--- RobotControl/TestCode/LightSensor_tester/LightSensor_tester.ino
+++ RobotControl/TestCode/LightSensor_tester/LightSensor_tester.ino
@@ -7,31 +7,21 @@ Zachary Feuerstein
 
 #define FSI A2
 
-int LeftSensor = 0;
-int RightSensor = 0;
 int FrontSensor = 0;
 
 void setup(){
   Serial.begin(9600);
   pinMode(FSI, INPUT);
-  //pinMode(RSI, INPUT);
-  //pinMode(FSI, INPUT);
+
 }
 
 void loop(){
 
-  LeftSensor = analogRead(FSI);//make sure you want will be outputing voltage from sensor
-  //RightSensor = analogRead(RSI);//between 5mv to 5v or this wont work
-  //FrontSensor = analogRead(FSI);
+  FrontSensor = analogRead(FSI);//make sure you want will be outputing voltage from sensor
   
-  Serial.print("\nLeft Sensor: ");
-  Serial.print(LeftSensor);
+  Serial.print("\nFront Sensor: ");
+  Serial.print(FrontSensor);
   
-  //Serial.print("\tRight Sensor: ");
-  //Serial.print(RightSensor);
-  
-  //Serial.print("\tFront Sensor: ");
-  //Serial.print(FrontSensor);
   delay(500);
   Serial.print("\n");
   
